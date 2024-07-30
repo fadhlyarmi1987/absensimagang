c60ff1063248bc6b21d6e62b155808c0c733328b
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2024-07-31 00:08:44 +0700

    refactor(DioException): error handling

--- lib/data/services/auth.service.dart
+++ lib/data/services/auth.service.dart
@@ -1,5 +1,7 @@
 import 'package:absensimagang/utils/api_constants.dart';
-import 'package:dio/dio.dart';
+import 'package:dio/dio.dart' as dio;
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '../providers/api.provider.dart';
 
@@ -8,31 +10,45 @@ class AuthService {
 
   Future<bool> login(Map<String, dynamic> data) async {
     try {
-      Response response = await httpClient.post(ApiConstants.login, data: data);
+      dio.Response response = await httpClient.post(ApiConstants.login, data: data);
       var responseBody = response.data;
       if (responseBody['metaData']['code'] == 200) {
         return true;
       } else {
         return false;
       }
-      
-    } on DioError catch(_) {
-      throw Exception(_.message);
+    } on dio.DioException catch(_) {
+      //throw Exception(_.message);
+      Get.snackbar(
+        'Login Gagal', 
+        'Silakan coba lagi!',
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+        snackPosition: SnackPosition.BOTTOM,
+      );
+      return false;
     }
   }
 
   Future<bool> register(Map<String, dynamic> data) async {
     try {
-      Response response = await httpClient.post(ApiConstants.register, data: data);
+      dio.Response response = await httpClient.post(ApiConstants.register, data: data);
       var responseBody = response.data;
       if (responseBody['metaData']['code'] == 200) {
         return true;
       } else {
         return false;
       }
-      
-    } on DioError catch(_) {
-      throw Exception(_.message);
+    } on dio.DioException catch(_) {
+      //throw Exception(_.message);
+      Get.snackbar(
+        'Register Gagal', 
+        'Silakan coba lagi!',
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+        snackPosition: SnackPosition.BOTTOM,
+      );
+      return false;
     }
   }
 
